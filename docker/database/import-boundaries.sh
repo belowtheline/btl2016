6cc06763a47802c9693aea304f8d195f2c827d42
Benno Rice <benno@jeamland.net>
2016-05-30 22:25:14 -0700

    Get the SRID right.

--- docker/database/import-boundaries.sh
+++ docker/database/import-boundaries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "CREATE EXTENSION postgis;" | psql -U $POSTGRES_USER $POSTGRES_DB
-ogr2ogr -f PostgreSQL -lco "GEOMETRY_NAME=geom" \
+ogr2ogr -f PostgreSQL -a_srs EPSG:4283 -lco "GEOMETRY_NAME=geom" \
     "PG:dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" \
     /aec_boundaries/COM_ELB.TAB
